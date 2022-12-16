87b8f731e9b4ce16264058c1a1f8d6827cb5aace
nsomazr <zreuben815@gmail.com>
2022-12-16 22:40:29 +0300

    endpoints models

---
+++ backend/server/endpoints/urls.py
@@ -0,0 +1,18 @@
+# backend/server/endpoints/urls.py file
+from django.urls import include, re_path
+from rest_framework.routers import DefaultRouter
+
+from endpoints.views import EndpointViewSet
+from endpoints.views import MLAlgorithmViewSet
+from endpoints.views import MLAlgorithmStatusViewSet
+from endpoints.views import MLRequestViewSet
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"endpoints", EndpointViewSet, basename="endpoints")
+router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
+router.register(r"mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
+router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+
+urlpatterns = [
+    re_path(r"^api/v1/", include(router.urls)),
+]
